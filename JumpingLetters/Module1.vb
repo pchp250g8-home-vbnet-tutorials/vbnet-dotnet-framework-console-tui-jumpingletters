a8215b968b126db61313a4d038eb7edd15ff7da3
Alexander <pchp250g8@tutamail.com>
2026-01-13 13:04:58 +0300

    Добавьте файлы проекта.

---
+++ JumpingLetters/Module1.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Threading
+
+Module Module1
+
+    Sub Main()
+        Dim strLine = "Hello,World!!! VBNet Conio Example!!!"
+        Dim nLen = strLine.Length
+        Console.Clear()
+        Console.CursorVisible = False
+        For i As Integer = 0 To nLen - 1
+            For j As Integer = 70 To 10 + i Step -1
+                Console.SetCursorPosition(10 + j, 10)
+                Console.Write(strLine(i) + " ")
+                Thread.Sleep(5)
+            Next j
+        Next i
+        Console.SetCursorPosition(10, 10)
+        Console.Write(strLine)
+        Console.SetCursorPosition(10, 11)
+        Console.Write("Press any key")
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ JumpingLetters/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("JumpingLetters")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("JumpingLetters")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("de067b41-6c4e-4885-a806-f9350381fed9")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
